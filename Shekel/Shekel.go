9af51a2a22b54ac68f0d1b7d94e20299cecef451
Philipp Altmann <philippaltmann@deap.io>
2017-08-26 16:45:22 +0200

    Finished Ci

--- Shekel/Shekel.go
+++ Shekel/Shekel.go
@@ -2,19 +2,18 @@ package Shekel
 
 import "math"
 
+// Upper Boundary for function input, use for random initialization
 const UpperBound float64 = 100
+
+// Upper Boundary for function input, use for random initialization
 const LowerBound float64 = -100
 
+//Shekel Benchmark function
 func EvaluateFitness(values []float64) (fitness float64) {
 	const numberOfMaxima = 5
 
 	A := [][]float64{{0.5, 0.25, 0.25, 0.75, 0.75}, {0.5, 0.25, 0.75, 0.25, 0.75}}
 	c := []float64{0.002, 0.005, 0.005, 0.005, 0.005}
-
-	//const numberOfMaxima = 1
-
-	//A := [][]float64{{0.5}, {0.5}}
-	//c := []float64{0.002}
 
 	fitness = 0
 	for i := 0; i < numberOfMaxima; i++ {
