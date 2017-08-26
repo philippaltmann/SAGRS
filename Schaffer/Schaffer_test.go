4e9bd4beab3d93ce5f093173fdd1a4fd554c817e
Philipp Altmann <philippaltmann@deap.io>
2017-08-26 15:57:55 +0200

    Modified tests & travis

--- Schaffer/Schaffer_test.go
+++ Schaffer/Schaffer_test.go
@@ -7,5 +7,6 @@ func TestEvaluateFitness(t *testing.T) {
 	testValues := []float64{420.96874636, 420.96874636}
 	testResult := EvaluateFitness(testValues)
 	//t.Error("Result: %f", testResult)
+	t.Logf("Result: %f", testResult)
 
 }
