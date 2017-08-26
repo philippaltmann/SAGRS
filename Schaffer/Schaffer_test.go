3fd17777170a20da82cb9717eab4832d51ea80bc
Philipp Altmann <philippaltmann@deap.io>
2017-08-26 15:54:57 +0200

    Modified Schaffer tests

--- Schaffer/Schaffer_test.go
+++ Schaffer/Schaffer_test.go
@@ -6,6 +6,6 @@ func TestEvaluateFitness(t *testing.T) {
 	//testValues := []float64{420.96874636, 420.96874636, 420.96874636, 420.96874636}
 	testValues := []float64{420.96874636, 420.96874636}
 	testResult := EvaluateFitness(testValues)
-	t.Error("Result: %f", testResult)
+	//t.Error("Result: %f", testResult)
 
 }
