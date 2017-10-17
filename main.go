9672517b85aa42dfff64fca39e691b15ea3b98f3
Philipp Altmann <philippaltmann@deap.io>
2017-10-17 19:00:21 +0200

    small changes

--- main.go
+++ main.go
@@ -2,29 +2,43 @@ package main
 
 import (
 	a "github.com/philipp-altmann/ContinuousBenchmarkOptimizer/ApproximationOptimizer"
-	"github.com/philipp-altmann/ContinuousBenchmarkOptimizer/Bohachevsky"
 	e "github.com/philipp-altmann/ContinuousBenchmarkOptimizer/EvaluationOptimizer"
 	o "github.com/philipp-altmann/ContinuousBenchmarkOptimizer/Options"
-	p "github.com/philipp-altmann/ContinuousBenchmarkOptimizer/Plotter"
+	"github.com/philipp-altmann/ContinuousBenchmarkOptimizer/Rosenbrock"
 )
+
+func FitnessTest(val []float64) float64 {
+	fitness := 0.0
+	for i := 0; i < len(val); i++ {
+		fitness += float64(i+1) * val[i]
+	}
+	fitness += 5
+	return fitness
+}
 
 func main() {
 	options := o.Options{
-		Cycles:              100000,
+		Cycles:              10000,
 		EvaluationRate:      100,
-		EvaluationPoolSize:  1000,
+		EvaluationPoolSize:  10,
 		PopulationSize:      100,
-		Dimensions:          32,
+		Dimensions:          20,
 		SelectionFactor:     0.6,
 		MutationFactor:      0.3,
 		RecombinationFactor: 0.4}
-	approximatedProgressE, approximatedProgressA := a.Optimize(options, Bohachevsky.EvaluateFitness)
-	evaluatedProgress := e.Optimize(options, Bohachevsky.EvaluateFitness)
+	//approximatedProgressE, approximatedProgressA :=
+	a.Optimize(options, Rosenbrock.EvaluateFitness)
+	e.Optimize(options, Rosenbrock.EvaluateFitness)
+	//p.WriteCSV(progressE, "evaluated")
+	//p.WriteCSV(progressA, "approximated")
 
-	evaluatedPlottable := p.ConvertProgress(evaluatedProgress)
+	//evaluatedProgress :=
+	//e.Optimize(options, FitnessTest)
+
+	/*evaluatedPlottable := p.ConvertProgress(evaluatedProgress)
 	approximatedPlottableE := p.ConvertProgress(approximatedProgressE)
 	approximatedPlottableA := p.ConvertProgress(approximatedProgressA)
 
-	p.Plot(evaluatedPlottable, approximatedPlottableE, approximatedPlottableA)
+	p.Plot(evaluatedPlottable, approximatedPlottableE, approximatedPlottableA)*/
 
 }
