b04e5cd84e5c266db2a982dd522f2d865ae24ee2
Philipp Altmann <philippaltmann@deap.io>
2017-11-13 16:10:36 +0100

    Added Conditioned logging, progress tracking and convergence meassures

--- ApproximationOptimizer/ApproximationOptimizer.go
+++ ApproximationOptimizer/ApproximationOptimizer.go
@@ -3,162 +3,182 @@ package ApproximationOptimizer
 import (
 	"encoding/csv"
 	"fmt"
-	"math"
 	"os"
 	"strconv"
+	"strings"
 
+	"github.com/buger/goterm"
 	a "github.com/philipp-altmann/ContinuousBenchmarkOptimizer/Approximation"
 	i "github.com/philipp-altmann/ContinuousBenchmarkOptimizer/Individual"
 	o "github.com/philipp-altmann/ContinuousBenchmarkOptimizer/Options"
 	g "github.com/philipp-altmann/ContinuousBenchmarkOptimizer/Population"
 )
 
-//var ApproximationMatrix *mat64.Dense
-var ApproximationMatrix a.RBFNet
-
-//var ApproximationMatrix mat64.Dense
-
-func Approximator(value []float64) float64 {
-	//predictionInputMatrix := mat64.NewDense(1, len(value), value)
-	//return n.Predict(ApproximationMatrix, predictionInputMatrix)
-	return ApproximationMatrix.Predict(value)
-	//return a.ApproximateRBF(ApproximationMatrix, value)
-	//return a.ApproximateFitness(value, ApproximationMatrix)
-}
-
-func Optimize(o o.Options, FitnessFunction func([]float64) float64) (progressEvaluated []float64, progressApproximated []float64) {
-	cycle := 0
+func Optimize(o o.Options, FitnessFunction func([]float64) float64, Approximator a.Approximator /*, initialPopulation g.Population*/) ( /*bestPool g.Population */ bestIndividual i.Individual, cycle int) {
 
 	//Init, Evaluate & Sort Evaluation Pool random
 	evaluationPool := g.InitRandomPopulation(o.EvaluationPoolSize, o.Dimensions)
 	evaluationPool.Evaluate(FitnessFunction)
 	evaluationPool.Sort()
 
-	//Calculate Approximation Matrix for LSM Approximation
-	//ApproximationMatrix = n.GetApproximator(evaluationPool)
-	//ApproximationMatrix = a.GetLSMApproximator(evaluationPool)
-	ApproximationMatrix = a.GetRBFApproximator(evaluationPool)
+	convergenceCycle := o.Cycles
+
+	/*if len(initialPopulation) == o.EvaluationPoolSize {
+		evaluationPool = initialPopulation
+	}*/
+	functionFile, _ := os.Create("FunctionsProgress.txt")
+	defer functionFile.Close()
+
+	//Update Approximator
+	Approximator.Update(evaluationPool)
+
+	var writer *csv.Writer
+	if o.WriteProgress {
+		file, _ := os.Create(o.ProgressFileName + ".csv")
+		defer file.Close()
+		writer = csv.NewWriter(file)
+		defer writer.Flush()
+		writer.Write([]string{"Cycle", "Best in Pool", "Approximation Success", "Approximation Error"})
+	}
 
 	//Init, Approximate & Sort Population
 	population := g.InitRandomPopulation(o.PopulationSize, o.Dimensions)
-	population.Evaluate(Approximator)
+	population.Evaluate(Approximator.Predict)
 	population.Sort()
 
-	//Optimize
-	for cycle < o.Cycles {
-
-		//TODO plot average fitness in evaluationPool
-
-		//Evaluate best in population & append to EvaluationPool
-		if cycle%o.EvaluationRate == 0 {
-			best := i.GenerateIndividual(population[0].Value)
-			best.Fitness = FitnessFunction(best.Value)
-
-			for i := 0; i < o.EvaluationRate; i++ {
-				progressEvaluated = append(progressEvaluated, best.Fitness)
-			}
-
-			evaluationPool = append(evaluationPool, best)
-			evaluationPool.Sort()
-
-			//Cut worst to keep Evaluation Pool Size Constant
-			//evaluationPool = evaluationPool[:len(evaluationPool)-1]
-
-			//Update Approximator
-			//ApproximationMatrix = n.GetApproximator(evaluationPool)
-			//return
-			ApproximationMatrix = a.GetRBFApproximator(evaluationPool)
-			//ApproximationMatrix = a.GetLSMApproximator(evaluationPool)
-
-			/*var distance float64
-			for i := 0; i < len(evaluationPool); i++ {
-				for j := i; j < len(evaluationPool); j++ {
-					distance += a.DistanceTo(evaluationPool[i].Value, evaluationPool[j].Value)
-				}
-			}
-			distance /= float64(len(evaluationPool))
-			distance /= float64(len(evaluationPool))*/
-
-			//Print Best
-			fmt.Printf("Best approximated at %f, evaluated at %f\n\tBest in pool at %f, Size of pool: %d\n", population[0].Fitness, best.Fitness, evaluationPool[0].Fitness, len(evaluationPool))
+	converging := 0 //0 for not number for unsuccessful cycles in a row
+	for cycle := 0; cycle < o.Cycles; cycle++ {
+		if o.ResetPool {
+			//Init, Approximate & Sort Population
+			population := g.InitRandomPopulation(o.PopulationSize, o.Dimensions)
+			population.Evaluate(Approximator.Predict)
+			population.Sort()
 		}
+
+		//Optimize Population for Suggestion using Approximator
+		for generation := 0; generation < o.EvaluationRate; generation++ {
+
+			//Select
+			newSize := o.PopulationSize - int(float64(o.PopulationSize)*o.SelectionFactor)
+			population = population[:newSize]
+
+			//Mutate
+			population.Mutate(o.MutationFactor)
+
+			//Recombine
+			population.Recombine(o.RecombinationFactor, o.PopulationSize)
+
+			//Fillup
+			population.Fillup(o.PopulationSize, o.Dimensions)
+
+			//Evaluate using Approximator Function
+			population.Evaluate(Approximator.Predict)
+
+			//Sort
+			population.Sort()
+		}
+
+		//Suggest
+		var suggestions []i.Individual
+		bestApproximated := population[0].Fitness
+		better := 0
+		worse := 0
+		for suggestion := 0; suggestion < o.SuggestToEvaluation; suggestion++ {
+			individual := i.GenerateIndividual(population[suggestion].Value)
+			individual.Fitness = FitnessFunction(individual.Value)
+			suggestions = append(suggestions, individual)
+			if individual.Fitness > evaluationPool[len(evaluationPool)-1].Fitness {
+				worse++
+			}
+			if individual.Fitness < evaluationPool[0].Fitness {
+				better++
+			}
+		}
+		evaluationPool = append(evaluationPool, suggestions...)
+
+		evaluationPool.Evaluate(FitnessFunction)
+		evaluationPool.Sort()
+
+		//Cut worst to keep Evaluation Pool Size Constant
+		evaluationPool = evaluationPool[:len(evaluationPool)-o.SuggestToEvaluation]
+
+		//Update Approximator
+		Approximator.Update(evaluationPool)
+
+		if o.Verbose {
+			//Print Stats
+			goterm.MoveCursor(1, 1)
+			betterString := goterm.Color(fmt.Sprintf("[%s]Better", getBetterLamp(better)), goterm.CYAN)
+			goterm.Printf("%s[%4d]\n", makeProgressBar(cycle, o.Cycles, 50, goterm.GREEN), cycle)
+			goterm.Printf("%s   Suggestion Success: %s\n", betterString, makeProgressBar(o.SuggestToEvaluation-worse, o.SuggestToEvaluation, 20, goterm.BLUE))
+			goterm.Printf("Best in Pool at %6.6f       |Approximated at %6.6f\n", evaluationPool[0].Fitness, Approximator.Predict(evaluationPool[0].Value))
+
+			goterm.Printf("Rejected %d of %d\n", worse, o.SuggestToEvaluation)
+			goterm.Flush() // Call it every time at the end of rendering
+		}
+
+		terminateWhenConverging := true
+		functionFile.WriteString(Approximator.GetFunction() + "\n\n")
+
+		if terminateWhenConverging {
+			if worse == o.SuggestToEvaluation || evaluationPool[0].Fitness == 0.0 {
+				if converging > 0 {
+					converging++
+					/*goterm.MoveCursor(4, 1)
+					goterm.Printf("Convergin for %d Cycles", converging)
+					goterm.Flush()*/
+				} else {
+					converging = 1
+					convergenceCycle = cycle
+				}
+			} else {
+				converging = 0
+				convergenceCycle = cycle
+			}
+		}
+		if converging == o.ConvergenceThreshold {
+			//return evaluationPool[0], cycle //evaluationPool
+		}
+
+		//goterm.MoveCursor(3, 1)
+		//goterm.Println(makeProgressBar(cycle, o.Cycles, 50))
 
 		//Select
-		newSize := o.PopulationSize - int(float64(o.PopulationSize)*o.SelectionFactor)
-		population = population[:newSize]
-
-		//Mutate
-		population.Mutate(o.MutationFactor)
-
-		//Recombine
-		population.Recombine(o.RecombinationFactor, o.PopulationSize)
+		/*population = population[o.SuggestToEvaluation:]
+		fmt.Println(len(population))
 
 		//Fillup
-		population.Fillup(o.PopulationSize, o.Dimensions)
+		population.Fillup(o.PopulationSize, o.Dimensions)*/
 
-		//Evaluate using Approximator Function
-		population.Evaluate(Approximator)
+		if o.WriteProgress {
+			//Write Progress to csv
+			line := make([]string, 4)
+			//Cycle
+			line[0] = strconv.Itoa(cycle)
 
-		//Sort
-		population.Sort()
+			//Best in Pool
+			line[1] = strconv.FormatFloat(evaluationPool[0].Fitness, 'E', -1, 64)
 
-		//Append best to Hisory
-		progressApproximated = append(progressApproximated, population[0].Fitness)
+			//Approximation Success
+			line[2] = strconv.FormatFloat((float64(o.SuggestToEvaluation-worse) / float64(o.SuggestToEvaluation)), 'E', -1, 64)
 
-		/*if population[0].Fitness == 0.0 {
-			fmt.Println(population[0])
-			//fmt.Println(mat64.Formatted(&ApproximationMatrix))
-			fmt.Println(cycle)
-			fmt.Println(population[len(population)-1])
-			return
-		}*/
+			//Approximation Error
+			line[3] = strconv.FormatFloat((bestApproximated - FitnessFunction(population[0].Value)), 'E', -1, 64)
 
-		cycle++
-	}
-	evaluationPool.Sort()
-	fmt.Print(ApproximationMatrix)
-	fmt.Print("\n")
-	fmt.Print(evaluationPool[0])
-	fmt.Print("\n")
-	fmt.Print(Approximator(evaluationPool[0].Value))
-	fmt.Print("\n")
-
-	//fmt.Println(mat64.Formatted(&ApproximationMatrix))
-	//fmt.Println(ApproximationMatrix)
-
-	file, _ := os.Create("quadraticTest3.csv")
-	defer file.Close()
-
-	writer := csv.NewWriter(file)
-	defer writer.Flush()
-
-	cx := -20.0
-	writer.Write([]string{"X", "Y", "Z"})
-	for cx <= 20 {
-		cy := -20.0
-
-		for cy <= 20 {
-			line := []string{toString(cx), toString(cy), toString(ApproximationMatrix.Predict([]float64{cx, cy}))}
 			writer.Write(line)
-			cy += 0.1
-		}
-		cx += 0.1
-	}
-
-	var distanceSum float64
-	for i := 0; i < len(evaluationPool); i++ {
-		for j := i; j < len(evaluationPool); j++ {
-			distanceSum += a.EuclideanDistance(evaluationPool[i].Value, evaluationPool[j].Value)
 		}
 	}
-	distanceSum = math.Sqrt(distanceSum)
-	fmt.Println(distanceSum)
-	distanceSum /= float64(len(evaluationPool))
-	fmt.Println(distanceSum)
-
-	return
+	return evaluationPool[0], convergenceCycle //evaluationPool
 }
 
-func toString(num float64) string {
-	return strconv.FormatFloat(num, 'f', 6, 64)
+func getBetterLamp(better int) string {
+	if better > 0 {
+		return "•"
+	}
+	return " "
+}
+
+func makeProgressBar(i, from, length int, color int) string {
+	done := int(float64(i) / float64(from) * float64(length))
+	return "[" + goterm.Color(strings.Repeat("=", done), color) + strings.Repeat("-", length-done) + "]"
 }
