220156680f4bd8de7c81cba9b5c5dbd49d4d519f
Philipp Altmann <philippaltmann@deap.io>
2018-01-27 17:41:00 +0100

    Updated Main

--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 package main
 
-import "github.com/philipp-altmann/ContinuousBenchmarkOptimizer/Benchmark"
+import (
+	"fmt"
+	"os"
+
+	"github.com/philipp-altmann/ContinuousBenchmarkOptimizer/Benchmark"
+)
 
 func main() {
-
-	//Benchmark.SimpleTest() //100, 7, 1
-	Benchmark.TestEvaluationRates(100, 7, 0) //100, 7, 1
-	//Benchmark.TestConvergence()
-
+	args := os.Args[1:]
+	if len(args) < 2 {
+		fmt.Println("Please Specify What you want to run (e.g. benchmark rates)")
+		return
+	}
+	switch args[0] {
+	case "benchmark":
+		switch args[1] {
+		case "rates":
+			rates := []int{0, 1, 2, 3, 4, 5, 6, 8, 16, 32, 64}
+			Benchmark.TestEvaluationRates(10, rates)
+		}
+	}
 }
