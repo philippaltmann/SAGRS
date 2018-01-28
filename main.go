1e1ec588821126426c1f3f43f03d0166eae1be02
Philipp Altmann <philippaltmann@deap.io>
2018-01-28 17:18:18 +0100

    added Rates Test Suite

--- main.go
+++ main.go
@@ -18,11 +18,28 @@ func main() {
 		switch args[1] {
 		case "rates":
 			if len(args) == 4 {
-				rates := []int{0, 1, 2, 3, 4, 5, 6, 8, 16, 32, 64}
+				//rates := []int{0, 1, 2, 3, 4, 5, 6, 8, 16, 32, 64}
+				rates := []int{8, 32, 64}
 				approximator := args[2]
 				objective := args[3]
-				Benchmark.TestEvaluationRates(10, rates, approximator, objective)
+				Benchmark.TestEvaluationRates(10, rates, approximator, objective, 1000)
+			} else {
+				evaluationRateTestSuite()
 			}
 		}
 	}
+}
+
+func evaluationRateTestSuite() {
+	//reset = ["Reset", "NoReset"]
+	approximators := []string{"LSM", "RBF"}
+	objectives := []string{"Bohachevsky", "Ackley", "Schwefel"}
+	rates := []int{0, 1, 2, 3, 4, 5, 6, 8, 16, 32, 64}
+	cycles := 400
+	for _, approximator := range approximators {
+		for _, objective := range objectives {
+			Benchmark.TestEvaluationRates(10, rates, approximator, objective, cycles)
+		}
+	}
+
 }
