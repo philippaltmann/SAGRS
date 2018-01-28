0cdd79da768c06535dee3d540d996f841ac5b3fa
Philipp Altmann <philippaltmann@deap.io>
2018-01-28 17:21:15 +0100

    Small Environment Changes

---
+++ Environment/Environment.go
@@ -0,0 +1,30 @@
+package Environment
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+//Environment settings
+type Environment struct {
+	Cycles              int     // = 5000
+	EvaluationRate      int     //= 100
+	SuggestToEvaluation int     // = 1
+	EvaluationPoolSize  int     // = 10
+	PopulationSize      int     // = 100
+	Dimensions          int     //= 2
+	SelectionFactor     float64 //= 0.6
+	MutationFactor      float64 //= 0.3
+	RecombinationFactor float64 //= 0.4
+	ProgressFileName    string
+	WriteProgress       bool
+	Verbose             bool
+	ResetPool           bool
+	Approximator        string
+	Objective           string
+}
+
+func (e Environment) Dump(path string) {
+	env, _ := json.Marshal(e)
+	ioutil.WriteFile(path, env, 0644)
+}
